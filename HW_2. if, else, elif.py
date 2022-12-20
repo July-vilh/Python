6c15c0e86deb37aae4593a467b0c4f018150a29d
July <106986983+July-vilh@users.noreply.github.com>
2022-12-20 17:29:24 +0300

    create file

---
+++ HW_2. if, else, elif.py
@@ -0,0 +1,130 @@
+# 1. Создать переменную int_item со значением 10
+int_item = 10
+print(int_item)
+
+#  2. Создать переменную comp_item со значением 18
+comp_item = 18
+print(comp_item)
+
+#  3. Создать переменную mult_int в которой умножите int_item на 2
+mult_int = int_item * 2
+print(mult_int)
+
+# 4. Создать переменную item_2 со значением True
+item_2 = True
+print(item_2)
+
+# 5. Создать переменную item_3 со значением False
+item_3 = False
+print(item_3)
+
+# 6. Создать переменную item_4 со значением 0
+item_4 = 0
+print(item_4)
+
+#  7. Создать переменную item_5 со значением 1
+item_5 = 1
+print(item_5)
+
+# 8. Создать переменную usd_item со значением ‘usd’
+usd_item = 'usd'
+print(usd_item)
+
+# 9. Создать переменную usd_usd_rate со значением 1
+usd_usd_rate = 1
+print(usd_usd_rate)
+
+# 10. Создать переменную eur_item со значением ‘eur’
+eur_item = 'eur'
+print(eur_item)
+
+# 11. Создать переменную usd_eur_rate со значением 0.86
+usd_eur_rate = 0.86
+print(usd_eur_rate)
+
+# 12. Создать переменную uah_item со значением ‘uah’
+uah_item = 'uah'
+print(uah_item)
+
+# 13. Создать переменную usd_uah_rate со значением 26.23
+usd_uah_rate = 26.23
+print(usd_uah_rate)
+
+# 14. Создать переменную chf_item со значением ‘chf’
+chf_item = 'chf'
+print(chf_item)
+
+# 15. Создать переменную usd_chf_rate со значением 0.91
+usd_chf_rate = 0.91
+print(usd_chf_rate)
+
+# 16. Создать переменную rub_item со значением ‘rub’
+rub_item = 'rub'
+print(rub_item)
+
+# 17. Создать переменную usd_rub_rate со значением 71.88
+usd_rub_rate = 71.88
+print(usd_rub_rate)
+
+# 18. Создать переменную byn_item со значением ‘byn’
+byn_item = 'byn'
+print(byn_item)
+
+# 19. Создать переменную usd_byn_rate со значением 2.46
+usd_byn_rate = 2.46
+print(usd_byn_rate)
+
+# 20. Сделать if в котором будет условие: если mult_int больше comp_item, то вывести в консоль
+# (“Переменная mult_int больше”, comp_item)
+if mult_int > comp_item:
+    print('Переменная mult_int больше', comp_item)
+
+# 21. Создать переменную div_int в которой разделить int_item на 2
+div_int = int_item/2
+print(div_int)
+
+# 22. Сделать if в котором будет условие: если div_int меньше comp_item, то вывести в консоль
+# (“Переменная div_int меньше”, comp_item)
+if div_int < comp_item:
+    print('Переменная div_int меньше', comp_item)
+
+# 23. Создать переменную item_1 в которой прибавить 10 к переменной int_item
+item_1 = int_item + 10
+print(item_1)
+
+# 24. Сделать if в котором будет условие: если item_1 меньше comp_item, то вывести в консоль
+# (“Переменная item_1 меньше”, comp_item), иначе, вывести в консоль (“Переменная item_1 больше или равна”, comp_item)
+if item_1 < comp_item:
+    print('Переменная item_1 меньше', comp_item)
+else:
+    print('Переменная item_1 больше или равна', comp_item)
+
+# 25. Сделать if в котором будет условие: если item_2, то вывести в консоль
+# ("Переменная item_2 = ”, item_2), иначе, вывести в консоль (“Переменная item_2 = ”, item_3)
+if item_2:
+    print('Переменная item_2 = ', item_2)
+else:
+    print('Переменная item_2 = ', item_3)
+
+# 26. Сделать if в котором будет условие: если item_3, то вывести в консоль
+# (“Переменная item_3 = ”, item_2), иначе, вывести в консоль (“Переменная item_3 = ”, item_3)
+if item_3:
+    print('Переменная item_3 = ', item_2)
+else:
+    print('Переменная item_3 = ', item_3)
+
+# 27. Сделать if в котором будет условие: если item_5, то вывести в консоль (“Переменная item_5 = ”, item_5),
+# иначе, вывести в консоль (“Переменная item_5 = ”, item_4)
+if item_5:
+    print('Переменная item_5 = ', item_5)
+else:
+    print('Переменная item_5 = ', item_4)
+
+# 28. Сделать if в котором будет условие: если item_4, то вывести в консоль (“Переменная item_4 = ”, item_5),
+# иначе, вывести в консоль (“Переменная item_4 = ”, item_4)
+if item_4:
+    print('Переменная item_4 = ', item_5)
+else:
+    print('Переменная item_4 = ', item_4)
+    
+#  29. Создать переменную currency_convertor со значением item_2
